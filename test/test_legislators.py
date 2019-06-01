5f72123385d38171e3390370e6de9001a0f94d3d
Bert Huang <berberhuang@gmail.com>
2019-06-02 00:11:09 +0800

    modify basic unit-test

--- test/test_legislators.py
+++ test/test_legislators.py
@@ -7,8 +7,6 @@ from app import app
 
 @pytest.fixture
 def client():
-    # turns all warnings into errors for this module
-
     client = app.test_client()
     yield client
 
@@ -16,5 +14,6 @@ def test_election_list(client):
     rv = client.get('/elections')
     jData = json.loads(rv.data)
 
+    assert 200 == rv.status_code
     assert "hello" in jData
     assert "world" == jData['hello']
